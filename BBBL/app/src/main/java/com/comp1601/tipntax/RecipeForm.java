11ead0ff15011dc79bc1508e44f3852094583db4
Matthew Bennett <mben2010@live.ca>
2019-02-17 08:18:56 -0500

    soem work done

---
+++ BBBL/app/src/main/java/com/comp1601/tipntax/RecipeForm.java
@@ -0,0 +1,13 @@
+package com.comp1601.tipntax;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class RecipeForm extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_recipe_form);
+    }
+}
